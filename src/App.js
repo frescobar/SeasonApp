417f9a0d143001f2c3fd5d6e7115b70a7e262fb3
frescobar <frescobar87@gmail.com>
2020-06-17 20:08:42 -0500

    Added the code to get the current position in the browser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function App() {
-  return <div></div>;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: "",
+      errorMessage: "",
+    };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        console.log(err);
+        this.setState({
+          errorMessage: err.message,
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.errorMessage && !this.state.lat && <h3>Loading...</h3>}
+        {this.state.lat ? (
+          <h3>Latitud: {this.state.lat}</h3>
+        ) : (
+          <h3>Error: {this.state.errorMessage}</h3>
+        )}
+      </div>
+    );
+  }
 }
